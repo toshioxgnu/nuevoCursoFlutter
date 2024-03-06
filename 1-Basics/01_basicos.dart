aa9c4c50e3db746552804d27ecd9ec05aeb41ee7
Jose Gonzalez riquelme <jgonzalezr5@outlook.es>
2024-03-06 17:14:10 -0300

    basics

---
+++ 1-Basics/01_basicos.dart
@@ -0,0 +1,20 @@
+void main() {
+  final String pokemon = 'Ditto';
+  final int hp = 100;
+  final bool isAlive = true;
+  final List<String> abilities = ['impostor'];
+  final sprites = <String>['ditto/front.png', 'ditto/back.png'];
+
+  dynamic errorMessage = 'hola';
+
+  print("""
+  
+  $pokemon
+  $hp
+  $isAlive
+  $abilities
+  $sprites
+  $errorMessage
+  
+  """);
+}
