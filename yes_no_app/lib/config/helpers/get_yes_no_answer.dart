36e33a11b6848e80a04dcc6df6e5d9e2d9084a02
Jose Gonzalez riquelme <jgonzalezr5@outlook.es>
2024-03-25 11:27:37 -0300

    saved project

---
+++ yes_no_app/lib/config/helpers/get_yes_no_answer.dart
@@ -0,0 +1,11 @@
+import 'package:dio/dio.dart';
+
+import '../../domain/entities/message.dart';
+
+class GetYesNoAnswer {
+  final _dio = Dio();
+
+  Future<Message> getAnswer() {
+    return;
+  }
+}
